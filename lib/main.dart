67c38a9e203a045bff1bb6ffe1e2ef5bf7d53ea6
ishaan1995 <ishaankakkar95@gmail.com>
2020-05-26 03:11:46 +0530

    [updated] app primary color

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Funny Memes',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
