083e74d8f7536ff478a6c9ffe3232a942711118f
Ishaan Kakkar <ishaankakkar95@gmail.com>
2020-06-02 00:54:25 +0530

    Fixed reverse timestamp

--- lib/posts_helper.dart
+++ lib/posts_helper.dart
@@ -2,6 +2,19 @@ import 'rest.dart';
 import 'package:flutter/foundation.dart';
 import 'extensions/global_ext.dart';
 
+extension Sort on List<Post> {
+  void orderBy(String field, {bool desc = false}) {
+    if (field != 'timestamp') {
+      throw Exception('Not supported orderby on field: $field');
+    }
+    if (desc) {
+      this.sort((User p1, User p2) => p2.timestamp.compareTo(p1.timestamp));
+    } else {
+      this.sort((User p1, User p2) => p1.timestamp.compareTo(p2.timestamp));
+    }
+  }
+}
+
 Future<List<Post>> mergePostsByTime({
   @required List<Post> currentPosts,
   @required List<Post> newPosts,
@@ -16,7 +29,7 @@ Future<List<Post>> mergePostsByTime({
       currentPosts.sublist(indexOverflow + 1, currentPosts.length);
 
   temp += newPosts;
-  temp.sort((Post p1, Post p2) => p1.timestamp.compareTo(p2.timestamp));
+  temp.orderBy('timestamp', desc: true);
   output += temp;
 
   return output;
